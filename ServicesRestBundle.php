7dc572cc62cb42f0e65f666887c1b14e62683626
Bruno Piccirilli <bruno.piccirilli@ritspa.it>
2017-03-02 23:23:28 +0100

    Modificata la classe bundle base

--- ServicesRestBundle.php
+++ ServicesRestBundle.php
@@ -4,6 +4,12 @@ namespace Services\Bundle\Rest;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle  as BaseBundle;
 
+/**
+ * Bundle class
+ *
+ * Class ServicesRestBundle
+ * @package Services\Bundle\Rest
+ */
 class ServicesRestBundle extends BaseBundle
 {
     /**
